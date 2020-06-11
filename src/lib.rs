d10ea3d214f252a43916b4a7dff161d668d72b84
Cabral <PCabralSoftware@users.noreply.github.com>
2020-06-11 12:33:22 -0300

    Implemented new features for Vector, added dot product.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![warn(missing_doc_code_examples)]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/mathrs/mathrs/master/logo.png")]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/mathrs/mathrs/master/favicon.ico")]
+#![doc(issue_tracker_base_url = "https://github.com/mathrs/mathrs/issues/")]
 
 //! <img src="https://raw.githubusercontent.com/mathrs/mathrs/master/logo.png" width="256px" height="256px">
 //!
@@ -39,7 +40,7 @@ pub const EULER_GAMMA: f64 = 0.5772156649015328606065120900824024310421593359399
 
 /// A dimensionless `[y, x]` point in a two-dimensional cartesian space.
 /// Has two parameters, `x`, and `y`, both are `f64`.
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone)]
 pub struct Point {
     x: f64,
     y: f64,
@@ -75,12 +76,32 @@ impl IndexMut<u8> for Point {
     }
 }
 
+/// You can compare two `Point` structs using `==`.
+/// ```rust
+/// assert_eq!(Point {x: 0.0, y: 0.0} == Point {x: 0.0, y: 0.0}, true)
+/// ```
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
 /// A `[y, x]` vector in a two-dimensional cartesian space.
 /// Has two parameters, `x`, and `y`, both are `f64`.
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vector {
     x: f64,
     y: f64,
+}
+
+/// You can compare two `Vector` structs using `==`.
+/// ```rust
+/// assert_eq!(Vector {x: 0.0, y: 0.0} == Vector {x: 0.0, y: 0.0}, true)
+/// ```
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
 }
 
 /// You can get the `x` and `y` values of a `Vector` using indexing with `[]`.
@@ -94,6 +115,20 @@ impl Index<u8> for Vector {
         match index {
             0 => &self.y,
             1 => &self.x,
+            _ => panic!("Index for Vector must be 0 for y or 1 for x."),
+        }
+    }
+}
+
+/// You can set the `x` and `y` values of a `Vector` using indexing with `[]` and `=`.
+/// ```rust
+/// assert_eq!(Vector {x: 0.0, y: 0.0}[0] = 1.0, Vector {x: 0.0, y: 1.0})
+/// ```
+impl IndexMut<u8> for Vector {
+    fn index_mut(&mut self, index: u8) -> &mut Self::Output {
+        match index {
+            0 => &mut self.y,
+            1 => &mut self.x,
             _ => panic!("Index for Vector must be 0 for y or 1 for x."),
         }
     }
@@ -205,6 +240,19 @@ impl Div<f64> for Vector {
     }
 }
 
+/// You can get the dot product of a `Vector` with another `Vector` by using the method `dot()`.
+/// ```rust
+/// let v1 = Vector {x: 1.0, y: 0.5};
+/// let v2 = Vector {x: 0.5, y: 1.0};
+/// assert_eq!(v1.dot(v2), 3.0)
+/// ```
+#[allow(dead_code)]
+impl Vector {
+    fn dot(&self, other: Vector) -> f64 {
+        (self.x + other.x) + (self.y + other.y)
+    }
+}
+
 /// All of the test cases for the `structs` and `functions` of **mathrs**.
 #[cfg(test)]
 mod mathrs_tests {
@@ -242,5 +290,10 @@ mod mathrs_tests {
             Vector { x: 9.99, y: 9.99 } / Vector { x: 9.99, y: 9.99 },
             Vector { x: 1.0, y: 1.0 }
         );
+
+        assert_eq!(
+            Vector { x: 1.0, y: 0.5 }.dot(Vector { x: 0.5, y: 1.0 }),
+            3.0
+        )
     }
 }
